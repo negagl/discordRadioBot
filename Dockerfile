26acf0c511a5e2c0c78672a127e9874a37ca26b1
Nega García <nega_gl@outlook.com>
2024-11-21 10:47:11 -0500

    v1.0

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM python
+FROM python:3.10-slim
 
 WORKDIR /app
 
 COPY . /app
+
+RUN apt update && apt install ffmpeg -y
 
 RUN pip install --no-cache-dir -r requirements.txt
 
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import threading
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -10,15 +8,12 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 RADIO_TIEMPO = os.getenv("RADIO_TIEMPO")
 
-print(f"Token: {TOKEN}")
-print(f"Radio Tiempo: {RADIO_TIEMPO}")
-
 bot = commands.Bot(command_prefix='*', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-  print(f'Bot conectado como {bot.user}!')
-  
+    print(f'Bot conectado como {bot.user}!')
+
 @bot.command()
 async def join(ctx):
     if ctx.author.voice:
@@ -40,19 +35,5 @@ async def leave(ctx):
         await ctx.send("Desconectado del canal de voz.")
     else:
         await ctx.send("No estoy en un canal de voz.")
-        
-class HealthCheckHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Bot is running")
 
-def run_http_server():
-    server = HTTPServer(("0.0.0.0", 8080), HealthCheckHandler)
-    server.serve_forever()
-
-# Ejecutar el servidor HTTP en un hilo separado
-threading.Thread(target=run_http_server, daemon=True).start()
-
-# Ejecutar el bot
 bot.run(TOKEN)
